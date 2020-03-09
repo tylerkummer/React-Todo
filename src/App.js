994d97b889e8d936cbb44aaf9a4ee5c295a960ec
Tyler Kummer <tylerkummer@protonmail.com>
2020-03-09 14:54:03 -0500

    tyler-kummer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import TodoList from './components/TodoList';
+
+const tasks = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  // this component is going to take care of state, and any change handlers you need to work with your state  
+
+  constructor(){
+    super();
+    this.state ={
+      tasks,
+      task: ''
+    }
+  }
+
+  toggleCompleted = clickedItemId => {
+    this.setState({
+      tasks: this.state.tasks.map(item => {
+        if(item.id === clickedItemId){
+          return {
+            ...item,
+            completed: !item.completed
+          }
+        } else{
+          return item
+        }
+      })
+    })
+  }
+
+  addItem = itemName => {
+    const newItem = {
+      task: itemName,
+      id: new Date(),
+      completed: false
+    }
+    this.setState({
+      tasks: [...this.state.tasks, newItem]
+    })
+  }
+
   render() {
     return (
       <div>
